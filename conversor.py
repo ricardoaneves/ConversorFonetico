93b06e3e1720215cdbb129c72e0f4f4e97de214f
ricardoaneves <ricardo.neves97@gmail.com>
2020-06-11 15:58:23 +0100

    Syllabic division working. Missing only capital letters.

--- conversor.py
+++ conversor.py
@@ -3,7 +3,7 @@
 import time
 
 # Vowels and consonants in the alphabet
-vowels = ['a', 'e', 'i', 'o', 'u', 'á', 'à', 'ã', 'â', 'é', 'ẽ', 'í', 'ó', 'õ', 'ú']
+vowels = ['a', 'e', 'i', 'o', 'u', 'á', 'à', 'ã', 'â', 'é', 'ẽ', 'ê', 'í', 'ó', 'õ', 'ú', 'A', 'E', 'I', 'O', 'U']
 consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'ç']
 
 # IPA vowels
@@ -32,26 +32,44 @@ def get_syllables(word):
 
         flag = 0
 
+        # Prefixos que terminam em b e d
+        if i == 0:
+            if (array[i] == 's' and array[i+1] == 'u' and array[i+2 == 'b']):
+
+                res.append(array[i])
+                res.append(array[i+1])
+                res.append(array[i+2])
+                i += 2
+                flag = 1
+
+            elif (array[i] == 'a' and array[i+1] == 'b') or \
+                (array[i] == 'a' and array[i+1] == 'd'):
+
+                res.append(array[i])
+                res.append(array[i+1])
+                i += 1
+                flag = 1
+
         # Hiatos e vogais duplas dividem-se em duas silabas diferentes
         if array[i] in vowels and array[i+1] in vowels:
 
             # Sequências que formam ditongos decrescentes
-            if  (array[i] == 'a' and array[i+1] == 'i') or \
-                (array[i] == 'a' and array[i+1] == 'u') or \
-                (array[i] == 'á' and array[i+1] == 'u') or \
-                (array[i] == 'ã' and array[i+1] == 'e') or \
-                (array[i] == 'ã' and array[i+1] == 'i') or \
-                (array[i] == 'ã' and array[i+1] == 'o'):
+            if (array[i] == 'a' and array[i+1] == 'i') or \
+               (array[i] == 'a' and array[i+1] == 'u') or \
+               (array[i] == 'á' and array[i+1] == 'u') or \
+               (array[i] == 'ã' and array[i+1] == 'e') or \
+               (array[i] == 'ã' and array[i+1] == 'i') or \
+               (array[i] == 'ã' and array[i+1] == 'o'):
 
                 res.append(array[i])
                 flag = 1
 
             # Sequências que formam ditongos decrescentes
-            elif  (array[i] == 'e' and array[i+1] == 'i') or \
-                  (array[i] == 'e' and array[i+1] == 'u') or \
-                  (array[i] == 'é' and array[i+1] == 'i') or \
-                  (array[i] == 'é' and array[i+1] == 'u') or \
-                  (array[i] == 'ê' and array[i+1] == 'i'):
+            elif (array[i] == 'e' and array[i+1] == 'i') or \
+                 (array[i] == 'e' and array[i+1] == 'u') or \
+                 (array[i] == 'é' and array[i+1] == 'i') or \
+                 (array[i] == 'é' and array[i+1] == 'u') or \
+                 (array[i] == 'ê' and array[i+1] == 'i'):
 
                 res.append(array[i])
                 flag = 1
@@ -60,13 +78,12 @@ def get_syllables(word):
             elif array[i] == 'i' and array[i+1] == 'u':
 
                 res.append(array[i])
-                i += 1
                 flag = 1
 
             # Sequências que formam ditongos decrescentes
-            elif  (array[i] == 'o' and array[i+1] == 'i') or \
-                (array[i] == 'o' and array[i+1] == 'u') or \
-                (array[i] == 'ó' and array[i+1] == 'i'):
+            elif (array[i] == 'o' and array[i+1] == 'i') or \
+                 (array[i] == 'o' and array[i+1] == 'u') or \
+                 (array[i] == 'ó' and array[i+1] == 'i'):
 
                 res.append(array[i])
                 flag = 1
@@ -78,7 +95,7 @@ def get_syllables(word):
                 flag = 1
 
             # Hiatos são indivisíveis quando ocorrem em silaba átona final
-            elif  ((array[i] == 'e' and array[i+1] == 'a') or \
+            elif ((array[i] == 'e' and array[i+1] == 'a') or \
                   (array[i] == 'e' and array[i+1] == 'o') or \
                   (array[i] == 'i' and array[i+1] == 'a') or \
                   (array[i] == 'i' and array[i+1] == 'e') or \
@@ -100,73 +117,64 @@ def get_syllables(word):
                 res.append('.')
                 flag = 1
 
-        # Com excecao nalguns casos de palavras compostas por prefixos que terminam em b e d
-        if (array[i] == 's' and array[i+1] == 'u' and array[i+2 == 'b']):
+        # As combinações 'qu' e 'gu' não são separáveis da vogal ou ditongo que lhes sucede
+        if (array[i] == 'q' and array[i+1] == 'u') or \
+           (array[i] == 'g' and array[i+1] == 'u'):
+
+            if i != 0:
+                res.append('.')
 
             res.append(array[i])
             res.append(array[i+1])
             i += 1
             flag = 1
 
-        elif (array[i] == 'a' and array[i+1] == 'b') or \
-             (array[i] == 'a' and array[i+1] == 'd'):
-
-            res.append(array[i])
-            flag = 1
-
-        # As combinações 'qu' e 'gu' não são separáveis da vogal ou ditongo que lhes sucede
-        elif  (array[i] == 'q' and array[i+1] == 'u') or \
-              (array[i] == 'g' and array[i+1] == 'u'):
-
-            res.append(array[i])
-            res.append(array[i+1])
-            res.append(array[i+2])
-            res.append('.')
-            i += 2
-            flag = 1
-
         # Consoantes seguidas de vogais permanecem em inicio da silaba (ataque)
-        elif array[i] in consonants and array[i+1] in vowels and i != 0 and res[-1] != '.':
+        elif array[i] in consonants and array[i+1] in vowels:
 
-            res.append('.')
+            if i != 0:
+                res.append('.')
+
             res.append(array[i])
             flag = 1
 
         # Consoantes seguidas de outra consoante permanecem em final de silaba
-        if array[i] in consonants and array[i+1] in consonants:
+        if array[i] in consonants and array[i+1] in consonants and flag == 0:
 
             # Grupos consonanticos não são divisiveis
-            if  (array[i] == 'b' and array[i+1] == 'l') or \
-                (array[i] == 'b' and array[i+1] == 'r') or \
-                (array[i] == 'c' and array[i+1] == 'l') or \
-                (array[i] == 'c' and array[i+1] == 'r') or \
-                (array[i] == 'd' and array[i+1] == 'l') or \
-                (array[i] == 'd' and array[i+1] == 'r') or \
-                (array[i] == 'f' and array[i+1] == 'l') or \
-                (array[i] == 'f' and array[i+1] == 'r') or \
-                (array[i] == 'g' and array[i+1] == 'l') or \
-                (array[i] == 'g' and array[i+1] == 'r') or \
-                (array[i] == 'p' and array[i+1] == 'l') or \
-                (array[i] == 'p' and array[i+1] == 'r') or \
-                (array[i] == 't' and array[i+1] == 'l') or \
-                (array[i] == 't' and array[i+1] == 'r') or \
-                (array[i] == 'v' and array[i+1] == 'l') or \
-                (array[i] == 'v' and array[i+1] == 'r'):
+            if (array[i] == 'b' and array[i+1] == 'l') or \
+               (array[i] == 'b' and array[i+1] == 'r') or \
+               (array[i] == 'c' and array[i+1] == 'l') or \
+               (array[i] == 'c' and array[i+1] == 'r') or \
+               (array[i] == 'd' and array[i+1] == 'l') or \
+               (array[i] == 'd' and array[i+1] == 'r') or \
+               (array[i] == 'f' and array[i+1] == 'l') or \
+               (array[i] == 'f' and array[i+1] == 'r') or \
+               (array[i] == 'g' and array[i+1] == 'l') or \
+               (array[i] == 'g' and array[i+1] == 'r') or \
+               (array[i] == 'p' and array[i+1] == 'l') or \
+               (array[i] == 'p' and array[i+1] == 'r') or \
+               (array[i] == 't' and array[i+1] == 'l') or \
+               (array[i] == 't' and array[i+1] == 'r') or \
+               (array[i] == 'v' and array[i+1] == 'l') or \
+               (array[i] == 'v' and array[i+1] == 'r'):
 
-                res.append('.')
+                if i != 0:
+                    res.append('.')
+                    
                 res.append(array[i])
                 res.append(array[i+1])
                 i += 1
                 flag = 1
             
             # Digrafos - sequencias de consoantes que representam um só som não se separam
-            if  ((array[i] == 'c' and array[i+1] == 'h') or \
-                 (array[i] == 'l' and array[i+1] == 'h') or \
-                 (array[i] == 'n' and array[i+1] == 'h') or \
-                 (array[i] == 'r' and array[i+1] == 'r')) and \
-                 res[-1] != '.':
+            if  (array[i] == 'c' and array[i+1] == 'h') or \
+                (array[i] == 'l' and array[i+1] == 'h') or \
+                (array[i] == 'n' and array[i+1] == 'h'):
 
-                res.append('.')
+                if i != 0:
+                    res.append('.')
+
                 res.append(array[i])
                 res.append(array[i+1])
                 i += 1
@@ -175,19 +183,13 @@ def get_syllables(word):
             # Sao indivisiveis os grupos consonanticos cz e ps em inicio de palavra
             if i == 0:
 
-                if  (array[i] == 'c' and array[i+1] == 'z') or \
-                    (array[i] == 'p' and array[i+1] == 's'):
+                if (array[i] == 'c' and array[i+1] == 'z') or \
+                   (array[i] == 'p' and array[i+1] == 's'):
 
                     res.append(array[i])
                     res.append(array[i+1])
                     i += 1
                     flag = 1
-
-            if flag == 0:
-
-                res.append(array[i])
-                res.append('.')
-                flag = 1
 
         # Se nenhuma regra foi aceite
         if flag == 0:
@@ -196,6 +198,13 @@ def get_syllables(word):
         i += 1
         
     res.append(array[i])
+
+    for i in range(len(res)):
+    
+        if res[i] == ' ':
+            if res[i+1] == '.':
+                res[i+1] = ''
+
     syllables = ''.join(res)
 
     return syllables
@@ -208,7 +217,6 @@ if __name__ == '__main__':
 
     print('\n' + 'Processing word...')
     syllables = get_syllables(word)
-    time.sleep(0.5)
     print()
 
     # Print results
